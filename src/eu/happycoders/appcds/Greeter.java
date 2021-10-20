4092490d965b172a0d5116ab1a7d3477c47ab7e1
Sven Woltmann <sven@happycoders.eu>
2021-10-20 20:24:17 +0200

    Initial commit

---
+++ src/eu/happycoders/appcds/Greeter.java
@@ -0,0 +1,8 @@
+package eu.happycoders.appcds;
+
+public class Greeter {
+
+  public void greet() {
+    System.out.println("Hello world!");
+  }
+}
---
+++ src/eu/happycoders/appcds/Main.java
@@ -0,0 +1,8 @@
+package eu.happycoders.appcds;
+
+public class Main {
+
+  public static void main(String[] args) {
+    new Greeter().greet();
+  }
+}
